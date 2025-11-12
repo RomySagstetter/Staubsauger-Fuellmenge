2adda8fb5dd19f5ab32dd509450d5fecf66e2b4f
RomySagstetter <romy.sagstetter@stud.th-deg.de>
2025-11-12 14:46:13 +0100

    Update and rename placeholder.md to Fuellstandsensor.java

---
+++ src/Füllstandsensor/Fuellstandsensor.java
@@ -0,0 +1,48 @@
+package Fuellstandsensor;
+
+/* misst Füllstand */
+public class Fuellstandsensor {
+
+    private static Fuellstandsensor single_instance = null;
+
+    private int fuellstand = 0;
+    private int maxFuellstand = 50;
+    private boolean notAus = false;
+
+    private Fuellstandsensor() {
+    }
+
+    public static synchronized Fuellstandsensor getInstance() {
+        if (single_instance == null)
+            single_instance = new Fuellstandsensor();
+        return single_instance;
+    }
+
+    public synchronized int getFuellstand() {
+        return fuellstand;
+    }
+
+    public synchronized void setFuellstand(int fuellstand) {
+        if (fuellstand < 0) fuellstand = 0;
+        if (fuellstand > maxFuellstand) fuellstand = maxFuellstand;
+        this.fuellstand = fuellstand;
+    }
+
+    public int getMaxFuellstand() {
+        return maxFuellstand;
+    }
+
+    /* vereinfacht: Not-Aus aktivieren */
+    public synchronized boolean NotAus() {
+        notAus = true;
+        return notAus;
+    }
+
+    /* nur wenn Füllbeutel gewechselt wurde: Reset */
+    public synchronized boolean stoppNotAus() {
+        fuellstand = 0;
+        notAus = false;
+        return notAus;
+    }
+}
+

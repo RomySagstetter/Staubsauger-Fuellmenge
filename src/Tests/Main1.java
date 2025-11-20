ad1e50a97e5e391f2869793c0e253825966aeacb
RomySagstetter <romy.sagstetter@stud.th-deg.de>
2025-11-20 18:14:47 +0100

    Update Main1.java

--- src/Tests/Main1.java
+++ src/Tests/Main1.java
@@ -4,15 +4,30 @@ import Anzeige.Anzeige;
 
 import javax.swing.SwingUtilities;
 
-public class Main1 {
+public class Main {
+	private static TestSprint1 test = new TestSprint1();
 
-    public static void main(String[] args) {
-        // GUI-Start auf Event Dispatch Thread
-        SwingUtilities.invokeLater(() -> {
-            Anzeige anzeige = Anzeige.getInstance();
-            anzeige.showAnzeige();
-        });
-    }
+	public static void main(String[] args) {
+		// GUI-Start auf Event Dispatch Thread
+		SwingUtilities.invokeLater(() -> {
+			Anzeige anzeige = Anzeige.getInstance();
+			anzeige.showAnzeige();
+		});
+
+		System.out.println("Test Mt1");
+		test.MT1();
+
+		System.out.println("Test Mt2");
+		test.MT2();
+
+		System.out.println("Test Mt3");
+		test.MT3();
+
+		System.out.println("Test It1");
+		test.IT1();
+
+		System.out.println("Test It2");
+		test.IT1();
+	}
 }
-
 

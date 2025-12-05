99c6838f3c4ddc755c0add286d8ad26cbc5fee46
RomySagstetter <romy.sagstetter@stud.th-deg.de>
2025-12-05 16:13:02 +0100

    Update Main1.java

--- src/Tests/Main1.java
+++ src/Tests/Main1.java
@@ -8,20 +8,27 @@ public class Main {
 	private static TestSprint1 test = new TestSprint1();
 
 	public static void main(String[] args) {
+
 		System.out.println("Test Mt1");
 		test.MT1();
 
-		System.out.println("Test Mt2");
-		test.MT2();
+		System.out.println("Test Mt4");
+		test.MT4();
 
 		System.out.println("Test Mt3");
 		test.MT3();
+		
+		System.out.println("Test MT5");
+		test.MT5();
+		
+		System.out.println("Test MT6");
+		test.MT6();
 
 		System.out.println("Test It1");
-		test.IT1();
-
-		System.out.println("Test It2");
-		test.IT1();
+		test.IT4();
+		
+		System.out.println("Test It6");
+		test.IT6();
 
 		// GUI-Start auf Event Dispatch Thread
 		SwingUtilities.invokeLater(() -> {
